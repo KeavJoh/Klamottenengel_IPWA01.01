58397ff52fae5bcf85d536c68890ab530330c5eb
Kevin Johr <kevin.johr@posteo.de>
2022-11-25 20:46:00 +0100

    create projekt and install bootstrap and fontawesome

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="bg-vue text-vue2"><h1>Hallo Welt</h1></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+</style>
