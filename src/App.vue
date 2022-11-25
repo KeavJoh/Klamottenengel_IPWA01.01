44a91260d5866f1e794622467236b64401a3d387
Kevin Johr <kevin.johr@posteo.de>
2022-11-25 20:51:41 +0100

    add css attributes for App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,19 @@ export default {
 <style>
 @import "~bootstrap/dist/css/bootstrap.min.css";
 @import "~@fortawesome/fontawesome-free/css/all.min.css";
+
+.bg-vue {
+  background-color: rgb(52, 73, 94);
+  color: white;
+}
+.bg-vue2 {
+  background-color: rgb(65, 184, 131);
+  color: white;
+}
+.text-vue {
+  color: rgb(52, 73, 94);
+}
+.text-vue2 {
+  color: rgb(65, 184, 131);
+}
 </style>
