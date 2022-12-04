ffce6bffb2b5f223034be0503d7ff4c9ecdfa964
Kevin Johr <kevin.johr@posteo.de>
2022-12-04 12:05:37 +0100

    implements form validation v.0.5

--- src/components/TheOfficeForm.vue
+++ src/components/TheOfficeForm.vue
@@ -2,9 +2,55 @@
   <div class="row mb-3">
     <h3 class="underline">Abholung bei Ihnen</h3>
     <p>Unsere Öffnungszeiten sind Mo - Fr von 15:00 Uhr bis 20:00 Uhr</p>
-    <Form @submit="submitData">
-      <TheCountryClothesForm></TheCountryClothesForm>
-      <button type="submit" class="btn btn-primary btn-block mb-4">
+    <Form @submit="submitData" :validation-schema="schema" v-slot="{ errors }">
+      <div>
+        <div>
+          <div class="row">
+            <div class="mt-4">
+              <p class="underline">
+                Bitte wählen Sie ein Kriesengebiet. Ist kein Kriesengebiet
+                ausgewählt, verwalten wir Ihre Spenden nach Bedarf der Gebiete.
+              </p>
+              <Field
+                as="select"
+                name="Zielland"
+                class="form-select mb-2"
+                aria-label="Kriesengebiet"
+              >
+                <option value="1">Mali</option>
+                <option value="2">Somalia</option>
+                <option value="3">Djibuti</option>
+                <option value="4">Ukraine</option>
+                <option value="5">Eritrea</option>
+                <option value="6">Burkina-Faso</option>
+                <option value="6">Nach Bedarf</option>
+              </Field>
+            </div>
+          </div>
+          <small class="text-danger" v-if="errors.Zielland">{{
+            errors.Zielland
+          }}</small>
+        </div>
+
+        <!-- Message input -->
+        <p class="underline mt-2">
+          Bitte teilen Sie uns kurz mit, was Sie Spenden möchten
+        </p>
+        <div class="form-outline mb-2">
+          <Field
+            as="textarea"
+            name="Informationen"
+            class="form-control"
+            id="AdditionalInformation"
+            rows="4"
+            placeholder="Beispiel: 2 Pullover, 3 paar Schuhe, 5 T-Shirt´s"
+          ></Field>
+        </div>
+        <small class="text-danger" v-if="errors.Informationen">{{
+          errors.Informationen
+        }}</small>
+      </div>
+      <button type="submit" class="btn btn-primary btn-block mb-4 mt-4">
         Place order
       </button>
     </Form>
@@ -12,14 +58,25 @@
 </template>
 
 <script>
-import TheCountryClothesForm from "../components/TheCountryClothesForm";
-import { Form } from "vee-validate";
+import { Form, Field } from "vee-validate";
+import * as yup from "yup";
 
 export default {
   name: "TheOfficeForm",
+  data() {
+    const schema = yup.object().shape({
+      Zielland: yup.string().required("Biite geben Sie ein Zielland ein "),
+      Informationen: yup
+        .string()
+        .required("Bitte teilen Sie uns kurz mit was Sie spenden möchten"),
+    });
+    return {
+      schema,
+    };
+  },
   components: {
-    TheCountryClothesForm,
     Form,
+    Field,
   },
   methods: {
     submitData(values) {
--- src/components/TheShippingForm.vue
+++ src/components/TheShippingForm.vue
@@ -1,9 +1,9 @@
 <template>
   <Form @submit="submitData" :validation-schema="schema" v-slot="{ errors }">
-    <div>{{ errors }}</div>
     <div>
       <div class="row mb-3">
         <h3 class="underline">Abholung bei Ihnen</h3>
+        <p>Nur innerhalb des PLZ Gebiet 28000 - 28999</p>
       </div>
       <!-- 2 column grid layout with text inputs for the first and last names -->
       <div class="row mb-4">
@@ -18,6 +18,9 @@
             />
             <label class="form-label" for="firstName">Vorname</label>
           </div>
+          <small class="text-danger" v-if="errors.Vorname">{{
+            errors.Vorname
+          }}</small>
         </div>
         <div class="col">
           <div class="form-outline">
@@ -30,69 +33,163 @@
             />
             <label class="form-label" for="lastName">Nachname</label>
           </div>
+          <small class="text-danger" v-if="errors.Nachname">{{
+            errors.Nachname
+          }}</small>
         </div>
       </div>
 
       <!-- Text input -->
       <div class="row">
         <div class="form-outline mb-4 col-md-6">
-          <Field
-            as="input"
-            name="Adresse"
-            type="text"
-            id="form6Example4"
-            class="form-control"
-          />
-          <label class="form-label" for="form6Example4"
-            >Straße und Hausnummer</label
-          >
+          <div>
+            <Field
+              as="input"
+              name="Adresse"
+              type="text"
+              id="form6Example4"
+              class="form-control"
+            />
+            <label class="form-label" for="form6Example4"
+              >Straße und Hausnummer</label
+            >
+          </div>
+          <small class="text-danger" v-if="errors.Adresse">{{
+            errors.Adresse
+          }}</small>
         </div>
         <div class="form-outline mb-4 col-md-4">
-          <Field
-            as="input"
-            name="Stadt"
-            type="text"
-            id="form6Example4"
-            class="form-control"
-          />
-          <label class="form-label" for="form6Example4">Stadt</label>
+          <div>
+            <Field
+              as="input"
+              name="Stadt"
+              type="text"
+              id="form6Example4"
+              class="form-control"
+            />
+            <label class="form-label" for="form6Example4">Stadt</label>
+          </div>
+          <small class="text-danger" v-if="errors.Stadt">{{
+            errors.Stadt
+          }}</small>
         </div>
         <div class="col-md-2">
-          <Field
-            as="input"
-            name="Postleitzahl"
-            type="text"
-            class="form-control"
-            id="inputZip"
-          />
-          <label for="inputZip" class="form-label">Postleitzahl</label>
+          <div>
+            <Field
+              as="input"
+              name="Postleitzahl"
+              type="text"
+              class="form-control"
+              id="inputZip"
+            />
+            <label for="inputZip" class="form-label">Postleitzahl</label>
+          </div>
+          <small class="text-danger" v-if="errors.Postleitzahl">{{
+            errors.Postleitzahl
+          }}</small>
         </div>
       </div>
       <!-- Number input -->
       <div class="form-outline">
-        <Field
-          as="input"
-          name="Telefonnummer"
-          type="text"
-          id="form6Example6"
-          class="form-control"
-        />
-        <label class="form-label" for="form6Example6">Telefonnumer</label>
+        <div>
+          <Field
+            as="input"
+            name="Telefonnummer"
+            type="text"
+            id="form6Example6"
+            class="form-control"
+          />
+          <label class="form-label" for="form6Example6">Telefonnummer</label>
+        </div>
+        <small class="text-danger" v-if="errors.Telefonnummer">{{
+          errors.Telefonnummer
+        }}</small>
       </div>
 
-      <TheDateTimePicker></TheDateTimePicker>
+      <div>
+        <div class="row">
+          <div class="mt-4">
+            <p class="underline">
+              Bitte wählen Sie einen Abholtermin Mo-Fr zwischen 09:00 Uhr und
+              20:00 Uhr
+            </p>
+              <Datepicker
+                id="DateTime"
+                v-model="date"
+                :clearable="true"
+                :no-today="true"
+                :disabled-week-days="[6, 0]"
+                week-start="0"
+                :min-time="{ hours: 9, minutes: 0 }"
+                :max-time="{ hours: 20, minutes: 0 }"
+              />
+          </div>
+        </div>
+      </div>
 
-      <TheCountryClothesForm></TheCountryClothesForm>
+      <div>
+        <div>
+          <div class="row">
+            <div class="mt-4">
+              <p class="underline">
+                Bitte wählen Sie ein Kriesengebiet. Ist kein Kriesengebiet
+                ausgewählt, verwalten wir Ihre Spenden nach Bedarf der Gebiete.
+              </p>
+              <Field
+                as="select"
+                name="Zielland"
+                class="form-select mb-2"
+                aria-label="Kriesengebiet"
+              >
+                <option value="1">Mali</option>
+                <option value="2">Somalia</option>
+                <option value="3">Djibuti</option>
+                <option value="4">Ukraine</option>
+                <option value="5">Eritrea</option>
+                <option value="6">Burkina-Faso</option>
+                <option value="6">Nach Bedarf</option>
+              </Field>
+            </div>
+          </div>
+          <small class="text-danger" v-if="errors.Zielland">{{
+            errors.Zielland
+          }}</small>
+        </div>
+
+        <!-- Message input -->
+        <p class="underline">
+          Bitte teilen Sie uns kurz mit, was Sie Spenden möchten
+        </p>
+        <div class="form-outline mb-2">
+          <Field
+            as="textarea"
+            name="Informationen"
+            class="form-control"
+            id="AdditionalInformation"
+            rows="4"
+            placeholder="Beispiel: 2 Pullover, 3 paar Schuhe, 5 T-Shirt´s"
+          ></Field>
+        </div>
+      </div>
+      <small class="text-danger" v-if="errors.Informationen">{{
+        errors.Informationen
+      }}</small>
     </div>
-    <button type="submit" class="btn btn-primary btn-block mb-4">
+    <button type="submit" class="btn btn-primary btn-block mt-4 mb-4">
       Place order
     </button>
   </Form>
 </template>
 
+<script setup>
+import { ref } from "vue";
+import Datepicker from "../../node_modules/@vuepic/vue-datepicker";
+import "../../node_modules/@vuepic/vue-datepicker/dist/main.css";
+
+const date = ref();
+</script>
+
 <script>
-import TheCountryClothesForm from "../components/TheCountryClothesForm";
-import TheDateTimePicker from "../components/TheDateTimePicker";
 import { Form, Field } from "vee-validate";
 import * as yup from "yup";
 
@@ -100,17 +197,37 @@ export default {
   name: "TheShippingForm",
   data() {
     const schema = yup.object().shape({
-      Vorname: yup.string().required(),
-    })
+      Vorname: yup
+        .string()
+        .required("Bitte geben Sie Ihren Vornamen an.")
+        .trim(),
+      Nachname: yup
+        .string()
+        .required("Bitte geben Sie Ihren Nachnamen an.")
+        .trim(),
+      Adresse: yup.string().required("Bitte geben Sie Ihre Adresse an.").trim(),
+      Stadt: yup.string().required("Bitte geben Sie Ihren Wohnort an").trim(),
+      Postleitzahl: yup
+        .number("Bitte verwenden Sie nur Zahlen")
+        .required("Bitte geben Sie Ihre Postleitzahl an")
+        .min(28000, "Keine gültige PLZ")
+        .max(28999, "Keine gültige PLZ"),
+      Telefonnummer: yup
+        .string()
+        .required("Bitte geben SIe Ihre Telefonnummer an"),
+      Zielland: yup.string().required("Biite geben Sie ein Zielland ein "),
+      Informationen: yup
+        .string()
+        .required("Bitte teilen Sie uns kurz mit was Sie spenden möchten"),
+    });
     return {
       schema,
     };
   },
   components: {
-    TheCountryClothesForm,
-    TheDateTimePicker,
     Field,
     Form,
+    Datepicker,
   },
   methods: {
     submitData(values) {
