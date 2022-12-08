06f84d108a7bbbd0f98418a3132a7c1dcf3f5f06
Kevin Johr <kevin.johr@posteo.de>
2022-12-08 10:20:52 +0100

    implement parnterCountries img

--- src/pages/PartnerCountries.vue
+++ src/pages/PartnerCountries.vue
@@ -1,18 +1,79 @@
 <template>
-  <TheOneColumnsLayout/>
+  <TheFreeColumsLayout>
+    <template #mainSlot>
+      <div class="container">
+        <div class="row ms-5 me-5">
+          <div class="col-md-4 mb-5">
+            <div class="card card-size">
+              <img src="../assets/Djibuti.png" alt="" />
+              <div class="card-body">
+                <p class="card-text text-center display-6">Djibuti</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card">
+              <img src="../assets/Mali.png" alt="" />
+              <div class="card-body">
+                <p class="card-text text-center display-6">Mali</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card">
+              <img src="../assets/Ukraine.png" alt="" />
+              <div class="card-body">
+                <p class="card-text text-center display-6">Ukraine</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row ms-5 me-5">
+          <div class="col-md-4 mb-5">
+            <div class="card">
+              <img src="../assets/Burkina_Faso.png" alt="" />
+              <div class="card-body">
+                <p class="card-text text-center display-6">Burkina-Faso</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card">
+              <img src="../assets/Eritrea.png" alt="" />
+              <div class="card-body">
+                <p class="card-text text-center display-6">Eritrea</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-5">
+            <div class="card">
+              <img src="../assets/Somalia.png" alt="" />
+              <div class="card-body">
+                <p class="card-text text-center display-6">Somalia</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </TheFreeColumsLayout>
 </template>
 
 <script>
-import TheOneColumnsLayout from "../layouts/TheOneColumnsLayout";
+import TheFreeColumsLayout from "../layouts/TheFreeColumsLayout";
 
 export default {
-name: "PartnerCountries",
-components: {
-    TheOneColumnsLayout,
-}
-}
+  name: "PartnerCountries",
+  components: {
+    TheFreeColumsLayout,
+  },
+};
 </script>
 
 <style scoped>
 
+.darc-size {
+  height: 60%;
+  width: 60%;
+}
 </style>
