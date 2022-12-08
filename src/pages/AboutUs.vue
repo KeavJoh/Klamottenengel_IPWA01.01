d2ef18e73da09989cc2677146d61811d9e5128a5
Kevin Johr <kevin.johr@posteo.de>
2022-12-08 11:27:09 +0100

    implement AboutUs final for beta

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,16 +1,334 @@
 <template>
-<TheOneColumnsLayout/>
+  <TheFreeColumsLayout>
+    <template #mainSlot>
+      <div class="container mt-5">
+        <section class="bg-light">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 mb-4 mb-sm-5">
+                <div class="card card-style1 border-0">
+                  <div class="card-body p-1-9 p-sm-2-3 p-md-6 p-lg-7">
+                    <div class="row align-items-center">
+                      <div class="col-lg-6 mb-4 mb-lg-0">
+                        <img
+                          src="../assets/Klamottenengel_Logo.png"
+                          alt="..."
+                        />
+                      </div>
+                      <div class="col-lg-6 px-xl-10">
+                        <div
+                          class="bg-secondary d-lg-inline-block py-1-9 px-1-9 px-sm-6 mb-1-9 rounded"
+                        >
+                          <h3 class="h2 text-vue2 mb-0">Klamottenengel e.V.</h3>
+                        </div>
+                        <ul class="list-unstyled mb-1-9">
+                          <li class="mb-2 mb-xl-3 display-28">
+                            <span
+                              class="display-26 text-secondary me-2 font-weight-600"
+                              >Email:</span
+                            >
+                            info@kevin-johr.dev
+                          </li>
+                          <li class="mb-2 mb-xl-3 display-28">
+                            <span
+                              class="display-26 text-secondary me-2 font-weight-600"
+                              >Website:</span
+                            >
+                            Domain folgt
+                          </li>
+                          <li class="display-28">
+                            <span
+                              class="display-26 text-secondary me-2 font-weight-600"
+                              >Phone:</span
+                            >
+                            01522 - 26 33 07 0
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-12 mb-4 mb-sm-5">
+                <div>
+                  <span class="section-title text-vue mb-3 mb-sm-4"
+                    >About Us</span
+                  >
+                  <p>
+                    Wir sind ein gemeinnütziger eingetragener Verein und kümmern
+                    uns um Sachspenden für gezielt ausgesuchte Partnerländer.
+                    Für den Transport werden wir von einem Logistikunternehmen
+                    aus Bremen unentgeltlich unterstützt.
+                  </p>
+                  <p class="mb-0">
+                    Entstanden ist dieser Verein, da leider immer mehr Schwarze
+                    Schaafe die Sachspenden nutzen, um sich gezeilt zu
+                    bereichern. Um dem entgegen zu Wirken haben wir ein Netzwerk
+                    aufgebaut, bestehend aus Unternehmen in Deutschland zur
+                    Finanziellen und Logistischen unterstützung aber auch
+                    Kontaktpersonen und eigenen Ausgabestellen in unseren
+                    Partnerländern.
+                  </p>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="row">
+                  <div class="col-lg-12 mb-4 mb-sm-5">
+                    <div class="mb-4 mb-sm-5">
+                      <span class="section-title text-vue mb-3 mb-sm-4"
+                        >Aktuelles Spendenaufkommen</span
+                      >
+                      <p class="text-vue">
+                        Bei 100% wird der Versand in das jeweilige Land
+                        vorbereitet und durchgeführt
+                      </p>
+                      <div class="progress-text">
+                        <div class="row">
+                          <div class="col-6">Mali</div>
+                          <div class="col-6 text-end">80%</div>
+                        </div>
+                      </div>
+                      <div
+                        class="custom-progress progress progress-medium mb-3"
+                        style="height: 4px"
+                      >
+                        <div
+                          class="animated custom-bar progress-bar slideInLeft bg-secondary"
+                          style="width: 80%"
+                          aria-valuemax="100"
+                          aria-valuemin="0"
+                          aria-valuenow="10"
+                          role="progressbar"
+                        ></div>
+                      </div>
+                      <div class="progress-text">
+                        <div class="row">
+                          <div class="col-6">Djibuti</div>
+                          <div class="col-6 text-end">90%</div>
+                        </div>
+                      </div>
+                      <div
+                        class="custom-progress progress progress-medium mb-3"
+                        style="height: 4px"
+                      >
+                        <div
+                          class="animated custom-bar progress-bar slideInLeft bg-secondary"
+                          style="width: 90%"
+                          aria-valuemax="100"
+                          aria-valuemin="0"
+                          aria-valuenow="70"
+                          role="progressbar"
+                        ></div>
+                      </div>
+                      <div class="progress-text">
+                        <div class="row">
+                          <div class="col-6">Burkina-Faso</div>
+                          <div class="col-6 text-end">50%</div>
+                        </div>
+                      </div>
+                      <div
+                        class="custom-progress progress progress-medium mb-3"
+                        style="height: 4px"
+                      >
+                        <div
+                          class="animated custom-bar progress-bar slideInLeft bg-secondary"
+                          style="width: 50%"
+                          aria-valuemax="100"
+                          aria-valuemin="0"
+                          aria-valuenow="70"
+                          role="progressbar"
+                        ></div>
+                      </div>
+                      <div class="progress-text">
+                        <div class="row">
+                          <div class="col-6">Ukraine</div>
+                          <div class="col-6 text-end">60%</div>
+                        </div>
+                      </div>
+                      <div
+                        class="custom-progress progress progress-medium mb-3"
+                        style="height: 4px"
+                      >
+                        <div
+                          class="animated custom-bar progress-bar slideInLeft bg-secondary"
+                          style="width: 60%"
+                          aria-valuemax="100"
+                          aria-valuemin="0"
+                          aria-valuenow="70"
+                          role="progressbar"
+                        ></div>
+                      </div>
+                      <div class="progress-text">
+                        <div class="row">
+                          <div class="col-6">Somalia</div>
+                          <div class="col-6 text-end">80%</div>
+                        </div>
+                      </div>
+                      <div
+                        class="custom-progress progress progress-medium mb-3"
+                        style="height: 4px"
+                      >
+                        <div
+                          class="animated custom-bar progress-bar slideInLeft bg-secondary"
+                          style="width: 80%"
+                          aria-valuemax="100"
+                          aria-valuemin="0"
+                          aria-valuenow="10"
+                          role="progressbar"
+                        ></div>
+                      </div>
+                      <div class="progress-text">
+                        <div class="row">
+                          <div class="col-6">Eritrea</div>
+                          <div class="col-6 text-end">80%</div>
+                        </div>
+                      </div>
+                      <div
+                        class="custom-progress progress progress-medium"
+                        style="height: 4px"
+                      >
+                        <div
+                          class="animated custom-bar progress-bar slideInLeft bg-secondary"
+                          style="width: 80%"
+                          aria-valuemax="100"
+                          aria-valuemin="0"
+                          aria-valuenow="10"
+                          role="progressbar"
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </template>
+  </TheFreeColumsLayout>
 </template>
 
 <script>
-import TheOneColumnsLayout from "../layouts/TheOneColumnsLayout";
+import TheFreeColumsLayout from "../layouts/TheFreeColumsLayout";
 
 export default {
   name: "AboutUs",
   components: {
-    TheOneColumnsLayout,
+    TheFreeColumsLayout,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.card-style1 {
+  box-shadow: 0px 0px 10px 0px rgb(89 75 128 / 9%);
+}
+.border-0 {
+  border: 0 !important;
+}
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+}
+
+section {
+  padding: 120px 0;
+  overflow: hidden;
+  background: #fff;
+}
+.mb-2-3,
+.my-2-3 {
+  margin-bottom: 2.3rem;
+}
+
+.section-title {
+  font-weight: 600;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  position: relative;
+  display: inline-block;
+}
+.text-primary {
+  color: #ceaa4d !important;
+}
+.text-secondary {
+  color: #15395a !important;
+}
+.font-weight-600 {
+  font-weight: 600;
+}
+.display-26 {
+  font-size: 1.3rem;
+}
+
+@media screen and (min-width: 992px) {
+  .p-lg-7 {
+    padding: 4rem;
+  }
+}
+@media screen and (min-width: 768px) {
+  .p-md-6 {
+    padding: 3.5rem;
+  }
+}
+@media screen and (min-width: 576px) {
+  .p-sm-2-3 {
+    padding: 2.3rem;
+  }
+}
+.p-1-9 {
+  padding: 1.9rem;
+}
+
+.bg-secondary {
+  background: rgb(52, 73, 94) !important;
+}
+@media screen and (min-width: 576px) {
+  .pe-sm-6,
+  .px-sm-6 {
+    padding-right: 3.5rem;
+  }
+}
+@media screen and (min-width: 576px) {
+  .ps-sm-6,
+  .px-sm-6 {
+    padding-left: 3.5rem;
+  }
+}
+.pe-1-9,
+.px-1-9 {
+  padding-right: 1.9rem;
+}
+.ps-1-9,
+.px-1-9 {
+  padding-left: 1.9rem;
+}
+.pb-1-9,
+.py-1-9 {
+  padding-bottom: 1.9rem;
+}
+.pt-1-9,
+.py-1-9 {
+  padding-top: 1.9rem;
+}
+.mb-1-9,
+.my-1-9 {
+  margin-bottom: 1.9rem;
+}
+@media (min-width: 992px) {
+  .d-lg-inline-block {
+    display: inline-block !important;
+  }
+}
+.rounded {
+  border-radius: 0.25rem !important;
+}
+</style>
